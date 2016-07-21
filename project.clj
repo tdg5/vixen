c05eff3a21a00f0ea2df84f056ccfe968f3465df
Isaac Cambron <isaac@isaaccambron.com>
2016-07-21 04:59:10 -0400

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vixen "0.1.0-SNAPSHOT"
+(defproject vixen "0.1.1-SNAPSHOT"
   :description "Manipulate DOM"
   :url "http://github.com/zensight/vixen"
   :plugins [[cider/cider-nrepl "0.12.0"]]
